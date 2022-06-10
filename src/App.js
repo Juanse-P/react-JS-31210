55659918b2e21ddc6b6ba63d94dffd621846512a
Juanse-P <jspp020205@gmail.com>
2022-06-10 10:57:16 -0500

    primeros pasos con react JS

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola locos</h1>
+      <p>como andamos, todo bien o que?</p>
+      <p>Hola Pablo, andamos entregando el primer desafio correctamente :D, ojala el resto tambien sean sencillos 🙏</p>
     </div>
   );
 }
